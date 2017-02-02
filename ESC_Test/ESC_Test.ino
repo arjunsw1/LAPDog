649bfaa8b3e0d393b1e4afa06dc111f114433f38
mmcdo005@ucr.edu <mmcdo005@ucr.edu>
2017-02-02 09:54:34 -0800

    Updated ESC/Motor Test Code
    Changed I/O pins to match new wiring

--- ESC_Test/ESC_Test.ino
+++ ESC_Test/ESC_Test.ino
@@ -1,17 +1,18 @@
 #include <Servo.h>
 
 Servo FL_ESC, FR_ESC, RL_ESC, RR_ESC; //Create Servo object for each ESC
-int Init_PB = 7;  // Flight initiation PB input
-int Init_LED = 8; // Flight initiation PB LED output
+int Init_PB = 2;  // Flight initiation PB input
+int Init_LED = 3; // Flight initiation PB LED output
+int Grn_LED = 12; // Green indicator LED
 
 void setup() {
   Serial.begin(9600);  // start serial at 9600 baud
   
   // ESC minimum speed = 1ms pulse width, ESC maximum speed = 2ms
-  FL_ESC.attach(9,1000,2000);    // Front left ESC attached to pin 9
-  FR_ESC.attach(5,1000,2000);    // Front left ESC attached to pin 5
+  FL_ESC.attach(7,1000,2000);    // Front left ESC attached to pin 9
+  FR_ESC.attach(4,1000,2000);    // Front left ESC attached to pin 5
   RL_ESC.attach(6,1000,2000);    // Front left ESC attached to pin 6
-  RR_ESC.attach(3,1000,2000);    // Front left ESC attached to pin 3
+  RR_ESC.attach(5,1000,2000);    // Front left ESC attached to pin 3
 }
 
 void setSpeed(int speed){
@@ -25,11 +26,13 @@ void setSpeed(int speed){
 void loop() {
   if(digitalRead(Init_PB)) {
     analogWrite(Init_LED, 255);
-    setSpeed(25);
+    analogWrite(Grn_LED, 255);
+    setSpeed(50);
   }
   
   else {
     analogWrite(Init_LED, 0);
+    analogWrite(Grn_LED, 0);
     setSpeed(0);
   }    
 }
